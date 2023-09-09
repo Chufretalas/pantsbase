5fb7131c5635739da85c471808567de344c94761
Chufretalas <marcobene.contact@gmail.com>
2023-09-09 19:08:56 -0300

    finished the new table form after a lot of js dom emocional manipulation

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/Chufretalas/pantsbase/db"
@@ -12,5 +13,6 @@ func main() {
 	defer db.DB.Close()
 
 	routes.LoadRoutes()
+	fmt.Println("Listening on http://localhost:8000")
 	http.ListenAndServe(":8000", routes.Router)
 }
